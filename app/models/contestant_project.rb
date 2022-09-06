1b18bf0d4f2e968124fc8bb1c894bf0e66e159b5
Madeline Mauser <madeline.mauser@gmail.com>
2022-09-06 14:31:38 -0600

    Add contestant count model test

--- app/models/contestant_project.rb
+++ app/models/contestant_project.rb
@@ -5,4 +5,7 @@ belongs_to :project
   def self.contestant_project_names(contestant_id)
     Project.joins(:contestant_projects).where(contestant_projects: {contestant_id: contestant_id}).pluck(:name).join(", ")
   end
+
+  def self.project_contestant_count(project_id)
+    ContestantProject.where(project_id: "#{project_id}").size
 end
--- spec/models/contestant_projects_spec.rb
+++ spec/models/contestant_projects_spec.rb
@@ -24,5 +24,20 @@ RSpec.describe ContestantProject, type: :model do
         expect(ContestantProject.contestant_project_names(@jay.id)).to eq("Litfit, News Chic")
       end
     end
+    describe '#project_contestant_count' do
+      it 'should return a count of contestants for a project' do
+        @furniture_challenge = Challenge.create(theme: "Apartment Furnishings", project_budget: 1000)
+
+        @lit_fit = @furniture_challenge.projects.create(name: "Litfit", material: "Lamp")
+
+        @jay = Contestant.create(name: "Jay McCarroll", age: 40, hometown: "LA", years_of_experience: 13)
+        @gretchen = Contestant.create(name: "Gretchen Jones", age: 36, hometown: "NYC", years_of_experience: 12)
+
+        ContestantProject.create(contestant_id: @jay.id, project_id: @lit_fit.id)
+        ContestantProject.create(contestant_id: @gretchen.id, project_id: @lit_fit.id)
+
+        expect(ContestantProject.project_contestant_count(@lit_fit.id)).to eq(2)
+      end
+    end
   end
 end
