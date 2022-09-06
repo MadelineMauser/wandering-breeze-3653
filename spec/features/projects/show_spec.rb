e4e02b50fbd8a4cbbd826b0e767c7c440e612252
Madeline Mauser <madeline.mauser@gmail.com>
2022-09-06 14:14:44 -0600

    Add contestant count test

--- spec/features/projects/show_spec.rb
+++ spec/features/projects/show_spec.rb
@@ -5,6 +5,12 @@ RSpec.describe 'project show page' do
     before :all do
       @furniture_challenge = Challenge.create(theme: "Apartment Furnishings", project_budget: 1000)
       @lit_fit = @furniture_challenge.projects.create(name: "Litfit", material: "Lamp")
+
+      @jay = Contestant.create(name: "Jay McCarroll", age: 40, hometown: "LA", years_of_experience: 13)
+      @gretchen = Contestant.create(name: "Gretchen Jones", age: 36, hometown: "NYC", years_of_experience: 12)
+
+      ContestantProject.create(contestant_id: @jay.id, project_id: @lit_fit.id)
+      ContestantProject.create(contestant_id: @gretchen.id, project_id: @lit_fit.id)
     end
     it 'should have the project name' do
       visit "/projects/#{@lit_fit.id}"
@@ -20,6 +26,11 @@ RSpec.describe 'project show page' do
       visit "/projects/#{@lit_fit.id}"
 
       expect(page).to have_content("Challenge Theme: Apartment Furnishings")
+    end
+    it 'should have a count of contestants on this project' do
+      visit "/projects/#{@lit_fit.id}"
+
+      expect(page).to have_content("Number of Contestants: 2")
     end
   end
 end
