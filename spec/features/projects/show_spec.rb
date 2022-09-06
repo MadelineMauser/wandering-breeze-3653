29f658f7656ac675bfe21810384e6d434acb9d96
Madeline Mauser <madeline.mauser@gmail.com>
2022-09-06 12:13:44 -0600

    Complete test for project show page

--- spec/features/projects/show_spec.rb
+++ spec/features/projects/show_spec.rb
@@ -3,14 +3,23 @@ require 'rails_helper'
 RSpec.describe 'project show page' do
   describe 'content' do
     before :all do
-      furniture_challenge = Challenge.create(theme: "Apartment Furnishings", project_budget: 1000)
-      lit_fit = furniture_challenge.projects.create(name: "Litfit", material: "Lamp")
+      @furniture_challenge = Challenge.create(theme: "Apartment Furnishings", project_budget: 1000)
+      @lit_fit = @furniture_challenge.projects.create(name: "Litfit", material: "Lamp")
     end
     it 'should have the project name' do
+      visit "/projects/:id"
+
+      expect(page).to have_content("Litfit")
     end
     it 'should have the project material' do
+      visit "/projects/:id"
+
+      expect(page).to have_content("Material: Lamp Shade")
     end
     it 'should have the project theme' do
+      visit "/projects/:id"
+
+      expect(page).to have_content("Challenge Theme: Apartment Furnishings")
     end
   end
 end
