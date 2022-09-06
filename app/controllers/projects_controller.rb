0fcc2536cd6af5af62b56314c2163884f6a49023
Madeline Mauser <madeline.mauser@gmail.com>
2022-09-06 14:37:53 -0600

    Add contestant count to project show page

--- app/controllers/projects_controller.rb
+++ app/controllers/projects_controller.rb
@@ -1,5 +1,6 @@
 class ProjectsController < ApplicationController
   def show
     @project = Project.find(params[:id])
+    @contestant_count = ContestantProject.project_contestant_count(params[:id])
   end
 end
